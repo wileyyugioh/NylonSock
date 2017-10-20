9767500bc44f014b37f304e8108791628b774caa
Wiley Y <nein@nein.nein>
2017-10-19 23:09:49 -0700

    enabled ipv6

--- NylonSock/src/Sustainable.h
+++ NylonSock/src/Sustainable.h
@@ -336,7 +336,7 @@ namespace NylonSock
             addrinfo hints = {0};
             //force server to be ipv6
             //ipv4 and ipv6 addresses can connect
-            hints.ai_family = AF_INET;
+            hints.ai_family = AF_INET6;
             hints.ai_socktype = SOCK_STREAM;
             hints.ai_protocol = IPPROTO_TCP;
             hints.ai_flags = AI_PASSIVE;
